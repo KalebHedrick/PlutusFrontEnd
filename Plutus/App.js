c716a77454d7468ead9a800e9480d962fcb12420
Kaleb Hedrick <destroycows@gmail.com>
2023-11-03 10:59:47 -0400

    added login screen

--- Plutus/App.js
+++ Plutus/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Login from './screens/Landing Page/loginScreen'
 export default function App() {
   return (
     <View style={styles.container}>
       
-      <Text>Open up App.js to start apple on your app!</Text>
+      <Login></Login>
       
       <StatusBar style="auto" />
     </View>
---
+++ Plutus/screens/Landing Page/loginScreen.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add api implementation for login here
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Plutus</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={text => setPassword(text)}
+      />
+      <TouchableOpacity style={styles.button} backgroundColor={"#DDDDDD"} flex={1} onPress={handleLogin}><Text style={styles.loginText}>Login</Text></TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100vh',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#69DC9E',
+    
+  },
+  title: {
+    fontSize: 64,
+    marginBottom: 20,
+  },
+  input: {
+    
+    height: "5%",
+    width: "20%",
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+  button: {
+    backgroundColor: "#000103",
+    color: "#FFFFFF",
+    borderRadius: 50,
+    width: '5%',
+    height: "5%",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginText: {
+    color: "#FFFFFF"
+  }
+});
+
+export default Login;
